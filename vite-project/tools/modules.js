871bde00002cbaae63dbb9db0f1980128dbc0450
kapy99 <kapibara2381@gmail.com>
2022-05-10 23:59:25 +0900

    ほぼ、json output done

--- vite-project/tools/modules.js
+++ vite-project/tools/modules.js
@@ -65,6 +65,7 @@ const HTMLParsedJSON = (htmlText,obj) => {
             obj.result[objName].status = "already";
             const {freeToppingList} = require("./data");
             resultArray = freeToppingList;
+            // console.log(obj,resultArray);
           default :
             break;
         }
@@ -74,12 +75,13 @@ const HTMLParsedJSON = (htmlText,obj) => {
           obj.result[objName].id = id;
           obj.result[objName].name = objName;
         }
-        console.log(`result:${objName}`,obj.result[objName].ary.length,resultArray.length);
+        // console.log((obj.result[objName].ary));
+        // console.log(`result:${objName}`,obj.result[objName].ary.length,resultArray.length);
       })
 
     }else{
       obj.result[obj.resultName] = Object.assign({});
-      obj.result[obj.resultName].ary = $(obj.getQuery);
+      obj.result[obj.resultName].ary = [].slice.call($(obj.getQuery));
       obj.result[obj.resultName].id = null;
       obj.result[obj.resultName].name = obj.resultName;
     }
@@ -98,15 +100,97 @@ price(size : regular || long) Number,
 糖質も一応 (size : regular || long) Number,
 kcal (size : regular || long) Number,
 */
-const parseItem = (resultObj) => {
-  if(resultObj.status === "skip") return;
-  if(resultObj.status === "already") {
-    resultObj.output = resultObj;
+/**
+ * @param {string} name - name
+ * @param {string} link - link path
+ * @param {Array}  size - Array<priceTemplate>
+*/
+const parentTemplate = {
+  name : "",
+  link : "",
+  size : []
+}
+/**
+ * @param {string} name - 主にサイズ名
+ * @param {number} price - 価格
+ * @param {number} carbohydrate - 糖質
+ * @param {number} kcal - カロリー
+*/
+const priceTemplate = {
+  name : "",
+  price : 0,
+  carbohydrate : 0,
+  kcal : 0,
+}
+const setItemInfo = (element,resultAry) => {
+  const temp = Object.assign({},parentTemplate);
+  const $ = cheerio.load(element);
+  temp.name = $(".product_name_ja").text();
+  if(!temp.name.length){
+    temp.name = $(".product_name h4").text();
   }
+  temp.link = $("li > a").attr("href");
+  temp.size = []
+
+  const priceAreaAry = Array.from($(".price_area"));
+  priceAreaAry.forEach(el => {
+    const obj = Object.assign({},priceTemplate);
+    const $$ = cheerio.load(el);
+    const priceName = $$(".price_name").text();
+    // if(!priceName || !priceName.length)return;
+
+    obj.name = priceName;
+    obj.price = Number($$(".price_yen").text().replace("￥","").replace("¥",""));
+    obj.carbohydrate = Number($$(".price_carb").text().replace("g","").replace("糖質 ",""));
+    obj.kcal = Number($$(".price_kcal").text().replace("kcal",""));
+
+    temp.size.push(obj);
+  })
+  resultAry.push(temp);
+}
+const parseItem = (resultObj) => {
+  if(resultObj.status === "skip" || !resultObj.ary.length){
+    delete resultObj.ary;
+    return;
+  }
+
+  resultObj.output = [];
+
+  if(resultObj.status === "already") {
+    resultObj.ary.forEach(item => {
+      const temp = Object.assign({},parentTemplate);
+      temp.name = item.name;
+      resultObj.output.push(temp);
+    });
+    delete resultObj.ary;
+    return;
+  }
+
+  for(var i = 0; i < resultObj.ary.length; i++){
+    const el = resultObj.ary[i];
+    setItemInfo(el,resultObj.output);
+  }
+  delete resultObj.ary;
 }
 
 const toJSON = (toJSONObj) => {
   //toJSONObjをjsonにして、リソースを更新する
+  const result = toJSONObj.slice();
+  result.forEach(page => {
+    const list = Object.keys(page.result);
+    if(!list.length) return;
+    list.forEach(key =>{
+      parseItem(page.result[key]);
+    })
+  })
+
+  //remove element
+  //convert
+  const resultPath = "data/output.json"
+  const { decycle } = require('json-cyclic');
+
+  fs.writeFile(resultPath,JSON.stringify(decycle(result),null,2),
+  (err) =>{ if(err) console.log(`error!::${err}`)});
 }
 
 const parseData = () => {
@@ -123,9 +207,8 @@ const parseData = () => {
       console.log(e.message);
     }
   })
-  console.log(result);
 
-
+  toJSON(result);
 }
 
 module.exports = {getHTMLData,parseData};

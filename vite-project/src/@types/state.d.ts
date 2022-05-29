54c6b98d276430604a0fc550417d15d36f9f7522
kapy99 <kapibara2381@gmail.com>
2022-05-29 14:27:50 +0900

    set redux dev tool

--- vite-project/src/@types/state.d.ts
+++ vite-project/src/@types/state.d.ts
@@ -7,3 +7,9 @@ interface InitializeSearchOption {
   name:string,
   flag:boolean,
 }
+// interface PayloadAction<P, T> {
+//   type: T;
+//   payload: P;
+//   meta ?: M;
+//   error ?: E;
+// }
--- vite-project/src/redux/store.ts
+++ vite-project/src/redux/store.ts
@@ -3,11 +3,19 @@ import { dataReducer } from "./MenuData/reducers";
 import { searchOptionsReducer } from "./searchOptions/reducers";
 
 
+//set type of window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+interface ExtendedWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
+declare var window: ExtendedWindow;
+const composeReduxDevToolsEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function createReduxStore(){
   return createStore(
     combineReducers({
       menuData:dataReducer,
       searchOptions:searchOptionsReducer,
     }),
+    composeReduxDevToolsEnhancers(applyMiddleware())
   )
 }

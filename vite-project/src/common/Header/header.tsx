34849a37decd1ce1d77858a7c152578204ee3c8c
kapy99 <kapibara2381@gmail.com>
2022-06-19 10:46:27 +0900

    fix resources path

--- vite-project/src/common/Header/header.tsx
+++ vite-project/src/common/Header/header.tsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react'
+
 
 export const Header = () => {
   return (
@@ -7,17 +7,17 @@ export const Header = () => {
         <ul className='u-header__links'>
           <li className="u-header__links-item">
             <a href="https://github.com/kapibara99" target="_blank" rel='noopener' className="u-header__links-content -github">
-              <img src="resources/images/icon/icon-sns-github.svg" alt="my facebook profile" />
+              <img src="/resources/images/icon/icon-sns-github.svg" alt="my facebook profile" />
             </a>
           </li>
           <li className="u-header__links-item">
             <a href="https://twitter.com/kapy99991" target="_blank" rel='noopener' className="u-header__links-content">
-              <img src="resources/images/icon/icon-sns-twitter.svg" alt="my twitter profile" />
+              <img src="/resources/images/icon/icon-sns-twitter.svg" alt="my twitter profile" />
             </a>
           </li>
           <li className="u-header__links-item">
             <a href="https://www.facebook.com/profile.php?id=100009267701448" target="_blank" rel='noopener' className="u-header__links-content">
-              <img src="resources/images/icon/icon-sns-facebook.svg" alt="my facebook profile" />
+              <img src="/resources/images/icon/icon-sns-facebook.svg" alt="my facebook profile" />
             </a>
           </li>
         </ul>

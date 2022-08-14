e9affe712c6105f425a552f4efc7485954843b22
kapy99 <kapibara2381@gmail.com>
2022-08-14 17:49:09 +0900

    add toml file seting redirect

--- vite-project/src/common/api/get.ts
+++ vite-project/src/common/api/get.ts
@@ -4,7 +4,7 @@ import { menuStringType } from "../../@types/element";
 export async function postData(url = '', data = {}) {
   // 既定のオプションには * が付いています
   const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
     // mode: 'cors', // no-cors, *cors, same-origin
     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     // credentials: 'same-origin', // include, *same-origin, omit
